5af78ca9684a5bef8ad62f992180702a6d327aac
Ronny Song <SongNL@users.noreply.github.com>
2019-03-20 01:17:37 +0800

    页面css

--- demo/Sudoku.js
+++ demo/Sudoku.js
@@ -1,22 +1,22 @@
-window.onload=initAll;
-function initall()
-{
-	document.getElementById("padding").onclick=1;
-	document.getElementById("vw").onclick=2;
-	document.getElementById("flex").onclick=3;
+function initbutton(){
+	function initall()
+	{
+		document.getElementById("padding").onclick=1;
+		document.getElementById("vw").onclick=2;
+		document.getElementById("flex").onclick=3;
+	}
+	switch(initall()){
+		case 1
+		var newcss=document.getElementById("linkid");
+	    newcss.href("href","https://songnl.github.io/demo/padding.css");
+	    break;
+	    case 2
+		var newcss=document.getElementById("linkid");
+	    newcss.href("href","https://songnl.github.io/demo/vw.css");
+	    break;
+	    case 3
+		var newcss=document.getElementById("linkid");
+	    newcss.href("href","https://songnl.github.io/demo/flex.css");
+	    break;
+	}
 }
-switch(initall()){
-	case 1
-	var newcss=document.getElementById("linkid");
-    newcss.href("href","https://songnl.github.io/demo/padding.css");
-    break;
-    case 2
-	var newcss=document.getElementById("linkid");
-    newcss.href("href","https://songnl.github.io/demo/vw.css");
-    break;
-    case 3
-	var newcss=document.getElementById("linkid");
-    newcss.href("href","https://songnl.github.io/demo/flex.css");
-    break;
-}
-	
